1cd76ef65b7864e93fc9683ef6d4b6ea38fda1c2
omeismm <128798266+omeismm@users.noreply.github.com>
2025-04-04 12:23:36 +0300

    Add conditional rendering and editable content feature
    Updated `app.component.html` to include an `if-else` statement for displaying messages based on the `isServerRunning` variable and a `for` loop to list user names. Added a `div` with the `contentEditable` attribute for editable content.
    
    Introduced `isEditable` property in `app.component.ts` to control the editability of the new content area.

--- AngularLearn/src/app/app.component.ts
+++ AngularLearn/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularLearn';
   isServerRunning = true;
+  isEditable = false;
   users = [{ id: 0, name: 'Sarah' }, { id: 1, name: 'Amy' }, { id: 2, name: 'Rachel' }, { id: 3, name: 'Jessica' }, { id: 4, name: 'Poornima' }]
 
 }
