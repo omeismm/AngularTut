720386c262d34f0704c08a171b80647aa3d5ec52
omeismm <128798266+omeismm@users.noreply.github.com>
2025-04-04 10:51:56 +0300

    Add project files.

---
+++ AngularLearn/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'AngularLearn';
+  isServerRunning = true;
+  users = [{ id: 0, name: 'Sarah' }, { id: 1, name: 'Amy' }, { id: 2, name: 'Rachel' }, { id: 3, name: 'Jessica' }, { id: 4, name: 'Poornima' }]
+
+}
