48a38d5f5a017555dd8946411830904691659223
omeismm <128798266+omeismm@users.noreply.github.com>
2025-04-05 11:43:39 +0300

    Add user and child components with interactivity
    Updated app.component.html to include new elements for toggling editability, displaying messages on mouseover, and integrating user and child components. Introduced message handling and item management in app.component.ts. Declared UserComponent and ChildComponent in app.module.ts. Created child.component.ts and user.component.ts to define their respective functionalities.

--- AngularLearn/src/app/app.component.ts
+++ AngularLearn/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'AngularLearn';
@@ -11,4 +11,18 @@ export class AppComponent {
   isEditable = false;
   users = [{ id: 0, name: 'Sarah' }, { id: 1, name: 'Amy' }, { id: 2, name: 'Rachel' }, { id: 3, name: 'Jessica' }, { id: 4, name: 'Poornima' }]
 
+  message = '';
+  edit() {
+    this.isEditable = !this.isEditable;
+  }
+  onMouseOver() {
+    this.message = 'Way to go 🚀';
+  }
+
+
+  items = new Array();
+
+  addItem(item: string) {
+    this.items.push(item);
+  }
 }
--- AngularLearn/src/app/app.module.ts
+++ AngularLearn/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UserComponent } from './user.component';
+import { ChildComponent } from './child.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
