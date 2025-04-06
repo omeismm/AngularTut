a4aa179000e9bc328a94a2232abd50a5580392cb
omeismm <128798266+omeismm@users.noreply.github.com>
2025-04-06 12:04:48 +0300

    Add SVG logo and enhance app components
    - Introduced `logo.svg` for branding.
    - Updated `app.component.html` with edit toggle button and mouseover functionality.
    - Imported `NgOptimizedImage` in `app.component.spec.ts` and `app.module.ts`.
    - Added `CommentsComponent` to display a list of comments.
    - Enhanced `user.component.ts` with additional user info and optimized image loading.
    - Added console log in `app.routes.ts` for debugging.

--- AngularLearn/src/app/app.module.ts
+++ AngularLearn/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgOptimizedImage } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
 import { ChildComponent } from './child.component';
+import { CommentsComponent } from './comments.component';
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    ChildComponent
+    ChildComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularLearn/src/app/user.component.ts
+++ AngularLearn/src/app/user.component.ts
@@ -1,11 +1,25 @@
 import { Component, Input } from '@angular/core';
-
 @Component({
   selector: 'app-user',
   template: `
     <p>The user's name is {{name}}</p>
+    <p>Username: {{ username }}</p>
+    <p>Preferred Framework:</p>
+    <ul>
+      <li>
+        Static Image:
+        <img ngSrc="logo.svg" alt="Angular logo"  width="32" height="32" />
+      </li>
+      <li>
+        Dynamic Image:
+        <img [ngSrc]="logoUrl" [alt]="logoAlt"  width="32" height="32" />
+      </li>
+    </ul>
     `,
 })
 export class UserComponent {
   @Input() name = '';
+  logoUrl = 'logo.svg';
+  logoAlt = 'Angular logo';
+  username = 'youngTech';
 }
