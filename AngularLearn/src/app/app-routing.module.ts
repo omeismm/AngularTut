6e0e79c7c8c0ebab229e148434e152a6400cfe6e
omeismm <128798266+omeismm@users.noreply.github.com>
2025-04-08 10:04:19 +0300

    fixed routing part 13

--- AngularLearn/src/app/app-routing.module.ts
+++ AngularLearn/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomehomeComponent } from './homehome/homehome.component';
+import { UseruserComponent } from './useruser/useruser.component';
+const routes: Routes = [
+  { path: '', component: HomehomeComponent },
+  { path: 'user', component: UseruserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
